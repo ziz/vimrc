eb59e92e68651b3c62ec72bba84288626bce1344
Justin de Vesine <ziz@ziz.org>
2012-04-17 12:50:45 -0600

    stick to develop where possible

--- vim/bundle/check_for_updates.sh
+++ vim/bundle/check_for_updates.sh
@@ -6,11 +6,7 @@ for submodule in *; do
 	if [ -e "$submodule/.git" ]; then
 		cd "$submodule"
 		echo "--- $submodule:"
-		if [ "$submodule" = "pyunit" ]; then
-			git checkout develop
-		else
-			git checkout master
-		fi
+		(git checkout develop || git checkout master) 2>/dev/null
 		git pull
 		cd ..
 	fi
